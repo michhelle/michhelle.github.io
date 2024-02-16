8eed8782608614b4926c19a86970be1a86b64501
michhelle <michellevu100@gmail.com>
2024-02-15 23:02:34 -0500

    personal website

---
+++ src/components/footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+      <div>
+        <div class="flex justify-center">
+          <a class="px-1" href="https://www.linkedin.com/in/michhelle/">
+            <img src="/linkedin.png" alt="linkedin" class="h-6 w-6" />
+          </a>
+
+          <a class="px-1" href="https://www.github./michhelle/">
+            <img src="/github.png" alt="github" class="h-6 w-6" />
+          </a>
+        </div>
+        <p class=" pt-1 text-center text-gray-500 text-xs">
+          &copy; 2024 michelle vu. all rights reserved.
+        </p>
+      </div>
+    );
+    }
---
+++ src/pages/about.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <p class="font-semibold pb-5">hi! my name is michelle</p>
+      <p>
+        i am currently a 4th year{" "}
+        <span class="font-semibold">computer science</span> student at toronto
+        metropolitan university (fka ryerson university).
+      </p>
+      <br />
+      <p>
+        i'm passionate about leveraging technology to{" "}
+        <span class="font-semibold">solve real world problems </span>
+        and improve people's lives.
+      </p>
+      <br />
+      <p>
+        i bring a positive attitude, a willingness to learn, and a strong work
+        ethic to any team i am a part of. i am eager to continue growing as a
+        software engineer and contributing to projects that have a meaningful
+        impact.
+      </p>
+      <br />
+      <p>
+        i am currently seeking internships or other opportunities to gain more
+        experience in the field. if you are interested in connecting or have any
+        opportunities to discuss,{" "}
+        <span class="font-semibold">please feel free to reach out</span>!
+      </p>
+    </div>
+  );
+}
